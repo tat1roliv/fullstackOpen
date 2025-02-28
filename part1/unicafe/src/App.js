a19fd4a8b532531b9b99a28ae557a21751b7841c
Tati Oliveira <tat1roliv@gmail.com>
2025-02-28 17:36:25 -0300

    part1-1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -19,29 +19,54 @@ const AveragePositiveFeedback = (good, neutral, bad) => {
   return good / TotalFeedbackColected(good, neutral, bad) * 100;  
 }
 
+// lugar adequado para definir um componente
+const Statistics = (props) => {
+  const allClicks = props.good + props.neutral + props.bad;
+  if(allClicks !== 0){
+    return (
+      <div>
+          <p>Good {props.good}</p>
+          <p>Neutral {props.neutral}</p>
+          <p>Bad {props.bad}</p>
+
+          <p>All {TotalFeedbackColected(props.good, props.neutral, props.bad)}</p>
+          <p>Average {AverageFeedback(props.good, props.neutral, props.bad)}</p>
+          <p>Average Positive feedback {AveragePositiveFeedback(props.good, props.neutral, props.bad)} %</p>
+      </div>
+    )
+  }
+}
+
 
 const App = () => {
-  // salve os cliques de cada botão em seu próprio estado
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const handleClickGood = () => {
+    setGood(good + 1);
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1);
+  }
+  
+  const handleClickBad = () => {
+    setBad(bad + 1);
+  }
 
   return (
     <div>
       <h1>Give feedback</h1>
       
-      <Button handleClick={() => setGood(good + 1)} text='good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Button handleClick={handleClickGood} text='good' />
+      <Button handleClick={handleClickNeutral} text='neutral' />
+      <Button handleClick={handleClickBad} text='bad' />
 
       <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-
-      <p>All {TotalFeedbackColected(good, neutral, bad)}</p>
-      <p>Average {AverageFeedback(good, neutral, bad)}</p>
-      <p>Average Positive feedback {AveragePositiveFeedback(good, neutral, bad)} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+     
     </div>
   )
 }
