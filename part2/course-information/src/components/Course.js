5016ba5070629f90cc29e7f02f5eed9314a39fe5
Tati Oliveira <tat1roliv@gmail.com>
2025-03-24 09:58:22 -0300

    part2.2.4+2.5

--- part2/course-information/src/components/Course.js
+++ part2/course-information/src/components/Course.js
@@ -6,9 +6,7 @@ const Header = (props) => {
 const Content = (props) => {
     return (
     <div>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />
+        {props.parts.map(part => <Part key={part.id} part={part} />)}
     </div>
 )}
 
