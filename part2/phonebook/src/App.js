a5871a531b001a8e08b00b3905347b86056423cb
Tati Oliveira <tat1roliv@gmail.com>
2025-03-31 09:36:04 -0300

    part2-2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      id: 1,
+     }
+  ]); 
+  const [newName, setNewName] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+  const handleNameChange = (event) => {
+    //console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form
+       onSubmit={addPerson}>
+        <div>
+          name:
+          <input 
+          value={newName}
+          onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
+      <ul>
+        {persons.map(person => 
+          <li key={person.id}>{person.id} - {person.name}</li>
+        )}
+      </ul> 
+    </div>
+  )
+}
+
+export default App
